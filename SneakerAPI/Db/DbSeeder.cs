60b9af7815e748df0cc89471b02e2cd12e636f86
patrick <patrick@makanda.io>
2021-06-14 14:36:59 +0200

    clean old context

--- SneakerAPI/Db/DbSeeder.cs
+++ SneakerAPI/Db/DbSeeder.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SneakerAPI.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SneakerAPI.Db
 {
@@ -11,12 +8,31 @@ namespace SneakerAPI.Db
     {
         public static void Seed(this ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Sneaker>().HasData(
-                    new Sneaker
-                    {
+            SeedSneakers(modelBuilder);
+        }
 
-                    }
-                );
+        private static void SeedSneakers(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Sneaker>().HasData(
+                                new Sneaker
+                                {
+                                    Id = Guid.NewGuid(),
+                                    Brand = "Nike",
+                                    Name = "Air Force 1",
+                                    ColorWay = "White",
+                                    ReleaseYear = new DateTime(1982, 1, 1),
+                                    Description = "Perhaps the most iconic Nike sneaker of all-time."
+                                },
+                                new Sneaker
+                                {
+                                    Id = Guid.NewGuid(),
+                                    Brand = "Adidas ",
+                                    Name = "Stan Smith",
+                                    ColorWay = "White",
+                                    ReleaseYear = new DateTime(1963, 1, 1),
+                                    Description = "The adidas Stan Smith tennis shoe design debuted in 1963 as the adidas Robert Haillet."
+                                }
+                            );
         }
     }
 }
