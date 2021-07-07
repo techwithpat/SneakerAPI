1e16cda6c6382aba19aa6dedcd9c4a056b56c242
patrick <patrick@makanda.io>
2021-07-07 17:20:46 +0200

    update

--- SneakerAPI/Controllers/SneakerController.cs
+++ SneakerAPI/Controllers/SneakerController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SneakerAPI.Models;
 using SneakerAPI.Services;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace SneakerAPI.Controllers
 {
+    [Produces("application/json")]
     [Route("api/sneakers")]
     [ApiController]
     public class SneakerController : ControllerBase
@@ -16,10 +18,31 @@ namespace SneakerAPI.Controllers
         public SneakerController(ISneakerService sneakerService) 
             => _sneakerService = sneakerService;
 
+        /// <summary>
+        /// Returns a list of sneakers
+        /// </summary>
+        /// <returns> A list of sneakers</returns>
+        /// <remarks>
+        /// 
+        /// Sample request
+        /// GET /api/sneakers
+        /// 
+        /// </remarks>
+        /// <response code="200">Returns a list of sneakers</response>
         [HttpGet, Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesDefaultResponseType]
         public IEnumerable<Sneaker> Get() => _sneakerService.GetAll();
 
+        /// <summary>
+        /// Create a new sneaker
+        /// </summary>
+        /// <param name="sneaker">Request's payload</param>
+        /// <returns></returns>
+        /// <response code="201">Sneaker created successfully</response>
         [HttpPost, Authorize(Roles = "admin")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesDefaultResponseType]
         public async Task<IActionResult> Post([FromBody] Sneaker sneaker)
         {
             await _sneakerService.Create(sneaker);
--- SneakerAPI/Startup.cs
+++ SneakerAPI/Startup.cs
@@ -3,8 +3,13 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using SneakerAPI.Db;
 using SneakerAPI.Extensions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 
 namespace SneakerAPI
 {
@@ -26,11 +31,55 @@ namespace SneakerAPI
             services.ConfigureJwt(Configuration);
             services.ConfigureServices();
             services.AddControllers();
+
+            services.AddSwaggerGen(s => 
+            {
+                s.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Version = "V1",
+                    Title = "Sneaker API",
+                    Description = "API for retrieving sneakers"
+                });
+
+                s.AddSecurityDefinition("bearer", new OpenApiSecurityScheme 
+                { 
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Scheme = "bearer"
+                });
+
+                s.AddSecurityRequirement(new OpenApiSecurityRequirement 
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            { 
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "bearer"
+                            }
+                        },
+                        new List<string>()
+                    }
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                s.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sneaker API V");
+                c.RoutePrefix = string.Empty;
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
